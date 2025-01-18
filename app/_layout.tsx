a3c1570902148381835bb5bab6acffcd3a6ab998
faleialina <august3@mail.ru>
2025-01-18 23:08:15 +0300

    fix themed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,3 +36,52 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
+
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+// import { useFonts } from 'expo-font'
+// import { Stack } from 'expo-router'
+// import * as SplashScreen from 'expo-splash-screen'
+// import { useEffect, useState } from 'react'; // Добавлено useState
+// import 'react-native-reanimated'
+
+// import { useColorScheme } from '@/hooks/useColorScheme'
+// import { Text, TouchableOpacity } from 'react-native'
+
+// // Prevent the splash screen from auto-hiding before asset loading is complete.
+// SplashScreen.preventAutoHideAsync();
+
+// export default function RootLayout() {
+//   const colorScheme = useColorScheme();
+//   const [loaded] = useFonts({
+//     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+//   });
+  
+//   // Добавлено состояние для хранения текущей темы
+//   const [theme, setTheme] = useState(colorScheme === 'dark' ? DarkTheme : DefaultTheme);
+
+//   useEffect(() => {
+//     if (loaded) {
+//       SplashScreen.hideAsync();
+//     }
+//   }, [loaded]);
+
+//   // Обработчик для переключения темы
+//   const toggleTheme = () => {
+//     setTheme(prevTheme => (prevTheme === DarkTheme ? DefaultTheme : DarkTheme));
+//   };
+
+//   if (!loaded) {
+//     return null;
+//   }
+
+//   return (
+//     <ThemeProvider value={theme}>         
+//       <TouchableOpacity style={{ backgroundColor: '#A1CEDC' }} onPress={toggleTheme} ><Text>Сменить тему</Text></TouchableOpacity>
+//       <Stack>
+//         <Stack.Screen name="(tabs)" options={{ headerTitle: 'Назад', headerShown: false }} />
+//         <Stack.Screen name="[topic]" options={{headerTitle: 'QA'}}/>
+//         <Stack.Screen name="+not-found" />
+//       </Stack>
+//     </ThemeProvider>
+//   );
+// }
